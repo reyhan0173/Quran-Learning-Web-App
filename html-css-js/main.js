9f90fafee9f716c4ec723240cbe3f9c5f2304161
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-05-13 01:01:06 -0600

    test

--- html-css-js/main.js
+++ html-css-js/main.js
@@ -1,7 +1,6 @@
 async function fetchAyahRecitation(parameters) {
-  const url = `https://api.quran.com/api/v4/${parameters}`;
-  console.log(url);
-  
+  const url = `https://api.quran.com/api/v4/recitations/3/by_ayah/${parameters}`;
+
   const response = await fetch(url);
   if (!response.ok) {
     throw new Error("Failed to fetch data: " + response.statusText);
@@ -10,22 +9,38 @@ async function fetchAyahRecitation(parameters) {
   return data;
 }
 
-async function playAudio(data_url, object) {
+async function playAudio(data_url) {
   const data = await fetchAyahRecitation(data_url);
-  const audio_data = data["audio_files"]
-  const audio_url = audio_data[0]["url"]
-  const audioPlayer = object.querySelector('#audioPlayer'); // Ensure this selector is correct
+  const audio_data = data["audio_files"];
+  const audio_url = audio_data[0]["url"];
 
-  console.log(`${audio_url}`)
-  audioPlayer.src = `https://verses.quran.com/${audio_url}`
-  console.log(audioPlayer)
-  audioPlayer.play()
+  audioPlayer.src = `https://verses.quran.com/${audio_url}`;
+  audioPlayer.play();
 }
 
-function playPauseAudio(object) {
-  data_url = object.getAttribute("data-url");
-  const myAudio = object.querySelector("#audioPlayer");
+async function fetchVerse(data_url) {
+  const response = await fetch(data_url);
+  if (!response.ok) {
+    throw new Error("Failed to fetch data: " + response.statusText);
+  }
+  const data = await response.json();
+  const verse = data.verses[0].text_indopak
+  return verse
+}
 
+async function showAyah(object) {
+  data_url = `https://api.quran.com/api/v4/quran/verses/indopak?verse_key=${object.getAttribute(
+    "data-url"
+  )}`;
+  const verse = await fetchVerse(data_url);
+  return verse
+}
+
+async function playPauseAudio(object) {
+  object.querySelector(".ayah").textContent = await showAyah(object);
+  data_url = object.getAttribute("data-url");
+  const myAudio = document.querySelector("#audioPlayer");
+  console.log(myAudio)
   if (!myAudio) {
     console.error("No audio player element found");
     return;
