16001a095b446cf4ffc18aba57adca7e0bca7d27
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-05-23 02:24:30 -0600

    documented bookmark functionalities

--- bookmarkFunctions.js
+++ bookmarkFunctions.js
@@ -1,5 +1,7 @@
-// isBookmarked(studentId, courseId, surahNumber, ayahNumber) returns 0 or 1
+// Use `Quran_Explorer`.`ayahFormatting` table
 
-// bookmarkAyah(studentId, courseId, surahNumber, ayahNumber) returns 1 if successfully added and console.logs "Successfully added a bookmark at {surahNumber:ayahNumber}" or logs the error and returns 0
+// isBookmarked(studentId, courseId, surahNumber, ayahNumber) returns 1 if bookmarked or 0 otherwise
+
+// isBookmarked(studentId, courseId, surahNumber, ayahNumber) returns 1 if successfully added and console.logs "Successfully added a bookmark at {surahNumber:ayahNumber}" or logs the error and returns 0
 
 // removeBookmark(studentId, courseId, surahNumber, ayahNumber) returns 1 if successfully removed and console.logs "Successfully removed the bookmark at {surahNumber:ayahNumber}" or logs the error and returns 0
