2625745c58d55926c97e0f63f3468d376cf27a6a
Oblivion0173 <113329952+Oblivion0173@users.noreply.github.com>
2024-06-27 12:47:09 -0600

    added sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import Header from './components/Header';
-import AyahList from './components/AyahList';
-import AudioRecorder from './components/AudioRecorder';
-import AudioPlayer from './components/AudioPlayer';
-import { AudioPlayerProvider } from './components/AudioPlayerContext';
-import 'font-awesome/css/font-awesome.min.css';
-import './App.css';
-import './main.css';
+import Sidebar from "./components/sidebar";
+import "./App.css";
+import {SidebarItem} from "./components/sidebar";
+import{
+    Lifebuoy,
+    Receipt,
+    Boxes,
+    Package,
+    UserCircle,
+    BarChart2,
+    LayoutDashboard,
+    Settings,
+} from "lucide-react";
 
-function App() {
-    return (
-        <AudioPlayerProvider>
-            <div className="App">
-                <Header />
-                <main>
-                    <AyahList start_pos="5:8" end_pos="5:10" />
-                    <AudioRecorder />
-                    <AudioPlayer />
-                </main>
-            </div>
-        </AudioPlayerProvider>
-    );
+export default function App() {
+  return (
+      <main className="App">
+          <Sidebar>
+              <SidebarItem
+                  icon={<LayoutDashboard size={20} />}
+                  text="Dashboard"
+                  alert
+              />
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active/>
+              
+          </Sidebar>
+      </main>
+  );
 }
-
-export default App;
