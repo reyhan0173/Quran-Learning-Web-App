94e903f25400122b2bf631fd5d473874775725c0
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-06-29 20:46:37 -0600

    sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -7,20 +7,46 @@ import { AudioPlayerProvider } from './components/AudioPlayerContext';
 import 'font-awesome/css/font-awesome.min.css';
 import './App.css';
 import './main.css';
+import Sidebar from "./components/Sidebar";
+import { SidebarItem } from "./components/Sidebar";
+import {
+  Lifebuoy,
+  Receipt,
+  Boxes,
+  Package,
+  UserCircle,
+  BarChart2,
+  LayoutDashboard,
+  Settings,
+} from "lucide-react";
 
 function App() {
-    return (
-      <AudioPlayerProvider>
-          <div className="App">
-              <Header/>
-              <main>
-                  <AyahList start_pos="5:8" end_pos="5:31"/>
-                  <AudioRecorder/>
-                  <AudioPlayer/>
-              </main>
-          </div>
-      </AudioPlayerProvider>
-    );
+  return (
+    <AudioPlayerProvider>
+      <div className="App">
+        <Header />
+        <div className="App-body">
+          <Sidebar>
+            <SidebarItem icon={<LayoutDashboard size={20} />} text="Dashboard" alert />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+            <SidebarItem icon={<BarChart2 size={20} />} text="Analytics" active />
+          </Sidebar>
+
+          <main className="App-main">
+            <AyahList start_pos="5:8" end_pos="5:31" />
+            <AudioRecorder />
+            <AudioPlayer />
+          </main>
+        </div>
+      </div>
+    </AudioPlayerProvider>
+  );
 }
 
 export default App;
