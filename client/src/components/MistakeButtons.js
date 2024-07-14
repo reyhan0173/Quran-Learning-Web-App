8394daf4d645a3632b85888ecca4a52cbd9df07f
hassanfarooq00 <hassanfarooq00@gmail.com>
2024-07-14 01:42:52 -0600

    added universal erase mistake button

--- client/src/components/MistakeButtons.js
+++ client/src/components/MistakeButtons.js
@@ -1,5 +1,38 @@
 import React, { useState } from "react";
 
+export const removeMistake = async (studentId, courseId, current_posStr, selectedText, start_idx, end_idx) => {
+
+  console.log(`${studentId}||${courseId}||${current_posStr}||${selectedText}||${start_idx}||${end_idx}`);
+  console.log(start_idx);
+
+  const mistakeIndexes = []
+  if (Number(start_idx) >= 0 && Number(end_idx) >= Number(start_idx)) {
+    console.log("loop begins");
+    for (let i = start_idx; i <= end_idx; i++) {
+      mistakeIndexes.push(i);
+    }
+  }
+
+  try {
+    console.log("_____DEBUG 14_____")
+    console.log(mistakeIndexes);
+
+    const response = await fetch("http://localhost:501/removeMistake", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ studentId, courseId, current_posStr, mistakeIndexes }),
+    });
+    if (!response.ok) {
+      throw new Error("Failed to add mistake");
+    }
+  } catch (error) {
+    console.error("Error adding mistake:", error);
+  }
+};
+
+
 export const addMistake = async (studentId, courseId, current_posStr, selectedText, start_idx, end_idx) => {
 
   console.log(`${studentId}||${courseId}||${current_posStr}||${selectedText}||${start_idx}||${end_idx}`);
@@ -49,6 +82,23 @@ const handleUniversalAddMistake = async () => {
   }
 };
 
+const handleUniversalRemoveMistake = async () => {
+  const { text, parentElement, current_posStr } = getSelectedText();
+  console.log(text, parentElement);
+  if (!text || !parentElement) return;
+
+  const verse = parentElement.querySelector('.ayah').textContent;
+
+  const studentId = 1111115; // Replace with actual studentId if needed
+  const courseId = 123; // Replace with actual courseId if needed
+
+  if (current_posStr && verse) {
+    const start_idx = verse.indexOf(text);
+    const end_idx = start_idx + text.length;
+    await removeMistake(studentId, courseId, current_posStr, text, start_idx, end_idx);
+  }
+};
+
 export default function MistakeButtons() {
   return (
     <div className="ayah-controls flex-row">
@@ -60,7 +110,7 @@ export default function MistakeButtons() {
       </button>
       <button
         className="ayah-remove-mistake-button"
-        // onClick={() => handleUniversalRemoveMistake()}
+        onClick={() => handleUniversalRemoveMistake()}
       >
         <i className={`fas fa-eraser`}></i>
       </button>
